a23e47d736b938c954841694743ffff7653913ba
Kavin Bala <kbalasubramani@bsd220.org>
2021-11-08 10:36:59 -0600

    Initial View

--- Roll the Dice/ContentView.swift
+++ Roll the Dice/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue = 0
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+        Text("Dice Roll")
+            .font(.title)
             .padding()
+            Text("\(randomValue)")
+            .font(.system(size: 72))
+                .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                }
+            Spacer()
     }
 }
 
@@ -18,4 +28,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
